e48f8b9e32e012ea328102262dd5cf040fc02933
Santi Ayechu <santiayechu@gmail.com>
2024-04-10 21:38:25 +0200

    [] - cleancode of class FizzBuzz

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -5,18 +5,43 @@ class FizzBuzz
 {
     public function execute(int $number): int|string
     {
-        if ($number % 3 == 0 && $number % 5 == 0) {
+        if ($this->isFizzBuzz($number)) {
             return 'FizzBuzz';
         }
 
-        if ($number % 3 == 0) {
+        if ($this->isFizz($number)) {
             return 'Fizz';
         }
 
-        if ($number % 5 == 0){
+        if ($this->isBuzz($number)){
             return 'Buzz';
         }
 
         return $number;
+    }
+
+    public function isFizzBuzz(int $number): bool
+    {
+        return $this->isFizz($number) && $this->isBuzz($number);
+    }
+
+    public function isFizz(int $number): bool
+    {
+        return $this->isDivisibleByThree($number);
+    }
+
+    public function isBuzz(int $number): bool
+    {
+        return $this->isDivisibleByFive($number);
+    }
+
+    public function isDivisibleByThree(int $number): bool
+    {
+        return $number % 3 == 0;
+    }
+
+    public function isDivisibleByFive(int $number): bool
+    {
+        return $number % 5 == 0;
     }
 }
